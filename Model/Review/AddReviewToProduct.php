3bbf73b0e720f722901b28151a2e791aa36d4ff7
Ростислав Сулейманов <rostilosfl@gmail.com>
2023-08-10 16:09:11 +0300

    added frontend and backend validation

--- Model/Review/AddReviewToProduct.php
+++ Model/Review/AddReviewToProduct.php
@@ -5,6 +5,7 @@ declare(strict_types=1);
 namespace Perspective\ReviewGraphQl\Model\Review;
 
 use Magento\Framework\GraphQl\Exception\GraphQlNoSuchEntityException;
+use Magento\Framework\Validator\ValidateException;
 use Perspective\Review\Model\ReviewFactory;
 
 /**
@@ -36,22 +37,30 @@ class AddReviewToProduct
      *
      * @return array
      *
-     * @throws GraphQlNoSuchEntityException
+     * @throws GraphQlNoSuchEntityException|ValidateException
      */
     public function execute(array $data, int $productId, ?int $customerId): array
     {
         $review = $this->reviewFactory->create()->setData($data);
         $review->unsetData('review_id');
 
-        try {
-            $review->setProductId($productId)
-                ->setCreatedAt(date('Y-m-d H:i:s'))
-                ->setUserId($customerId)
-                ->save();
-
-            return ['review' => $review, 'message' => 'Review Added'];
-        } catch (\Exception $e) {
-            throw new GraphQlNoSuchEntityException(__('Something went wrong.'));
+        $validate = $review->validate();
+        if ($validate === true) {
+            try {
+                $review->setProductId($productId)
+                    ->setCreatedAt(date('Y-m-d H:i:s'))
+                    ->setUserId($customerId)
+                    ->save();
+                return ['review' => $review->getData(), 'message' => 'Review Added'];
+            } catch (\Exception $e) {
+                throw new GraphQlNoSuchEntityException(__('Something went wrong.'));
+            }
+        } else {
+            if (is_array($validate)) {
+                return ['review' => null, 'message' => $validate[0]];
+            } else {
+                return ['review' => null, 'message' => __('We can\'t post your review right now.')];
+            }
         }
     }
 }
